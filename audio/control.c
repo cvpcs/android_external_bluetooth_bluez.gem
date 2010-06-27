ca4b3c34c3b608469d68232176a937023a716a2d
cvpcs <root@cvpcs.org>
2010-06-27 16:28:07 -0500

    fixed control connection race condition [cyanogen]
    Change-Id: I35d7d1822d52155fb10ca19e050eeb1506761038

--- audio/control.c
+++ audio/control.c
@@ -710,8 +710,11 @@ static void avctp_confirm_cb(GIOChannel *chan, gpointer data)
 	control = dev->control;
 
 	if (control->io) {
-		error("Refusing unexpected connect from %s", address);
-		goto drop;
+		/* We already have a control connection, and the device is trying to open
+		 * a new one.  Refuse it and keep the existing one. */
+		debug("Refusing control connect %p from %s (io %p already exists)", chan, address, control->io);
+		g_io_channel_shutdown(chan, TRUE, NULL);
+		return;
 	}
 
 	avctp_set_state(control, AVCTP_STATE_CONNECTING);
