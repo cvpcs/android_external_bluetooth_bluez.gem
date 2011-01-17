d184c0e8fcd5fa6d07596f4852bf1cd8d90f068f
cvpcs <root@cvpcs.org>
2011-01-16 18:48:35 -0600

    Revert "static libbluetooth"
    This reverts commit f7c7d26ad3071190f0696f03f08d58d6fb3cf65b.

--- lib/Android.mk
+++ lib/Android.mk
@@ -14,14 +14,3 @@ LOCAL_MODULE:=libbluetooth
 LOCAL_CFLAGS+=-O3
 
 include $(BUILD_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES:= \
-	bluetooth.c \
-	sdp.c \
-	hci.c
-LOCAL_C_INCLUDES+= \
-	 $(LOCAL_PATH)/../include/
-LOCAL_MODULE:=libbluetooth
-LOCAL_CFLAGS+=-O3
-include $(BUILD_STATIC_LIBRARY)
